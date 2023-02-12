e6219e02cd1ae869d8ea5e4b561982a779ce67ed
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-12 01:08:48 -0700

    made a new JS file for username/score storage

---
+++ storage.js
@@ -0,0 +1,6 @@
+localStorage.setItem("Score", correctSelection);
+localStorage.setItem("User", initalsInput.value);
+
+
+let userScore = localStorage.getItem("Score");
+let userName = localStorage.getItem("User")
